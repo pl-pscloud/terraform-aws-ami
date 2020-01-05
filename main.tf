4b60a608db6dd87ff2fa737fbbbda80adfba1e30
pswitala <p.switala@pslab.eu>
2020-01-05 14:06:36 +0100

    Added tags

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@
 resource "aws_ami_from_instance" "pscloud-ami-from-instance" {
   name                      = "${var.pscloud_company}-${var.pscloud_env}-${var.pscloud_purpose}-golden-ami"
   source_instance_id        = var.pscloud_instance_id
+
+  tags = {
+    Name = "${var.pscloud_company}_ami_${var.pscloud_env}"
+  }
 }
